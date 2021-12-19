a07c61f7fc1a425158afa5cf54e725ed9d0b50a4
Nikk <nikkigazejr@gmail.com>
2021-12-19 05:39:32 +0300

    Healthbar added

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,20 +6,27 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [SerializeField] private GameObject _healthBar;
     [SerializeField] private int _maxHP;
-    private int currentHP;
+    private int _currentHP;
     private bool _isAlive;
 
     private void Start()
     {
-        currentHP = _maxHP;
+        _currentHP = _maxHP;
         _isAlive = true;
     }
 
     public void TakeDamage(int count)
     {
-        currentHP -= count;
-        if (currentHP <= 0)
+        _currentHP -= count;
+        
+        SpriteRenderer renderer = _healthBar.GetComponent<SpriteRenderer>();
+        var rendererColor = renderer.color;
+        rendererColor.a = (float)_currentHP / _maxHP;
+        renderer.color = rendererColor;
+        
+        if (_currentHP <= 0)
         {
             _isAlive = false;
             Destroy(gameObject);
